74d195b5297efd705091fd43a1cc1451d299667e
Mikola Lysenko <mikolalysenko@gmail.com>
2014-10-17 19:27:02 -0500

    prevent division by 0

--- index.glsl
+++ index.glsl
@@ -15,8 +15,8 @@ float cookTorranceSpecular(
 
   //Geometric term
   float NdotH = max(dot(surfaceNormal, H), 0.0);
-  float VdotH = max(dot(viewDirection, H), 0.0001);
-  float LdotH = max(dot(lightDirection, H), 0.0001);
+  float VdotH = max(dot(viewDirection, H), 0.000001);
+  float LdotH = max(dot(lightDirection, H), 0.000001);
   float G1 = (2.0 * NdotH * VdotN) / VdotH;
   float G2 = (2.0 * NdotH * LdotN) / LdotH;
   float G = min(1.0, min(G1, G2));
@@ -28,7 +28,7 @@ float cookTorranceSpecular(
   float F = pow(1.0 - VdotN, fresnel);
 
   //Multiply terms and done
-  return  G * F * D / (3.14159265 * VdotN);
+  return  G * F * D / max(3.14159265 * VdotN, 0.000001);
 }
 
 #pragma glslify: export(cookTorranceSpecular)
